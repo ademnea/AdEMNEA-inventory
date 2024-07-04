3227f2ee0bf776fcf9141311a31b996d31c3cb84
joshuamumbere <joshuamumbere71@gmail.com>
2024-07-04 13:37:00 +0300

    Latest server changes

--- inventory/resources/views/items.blade.php
+++ inventory/resources/views/items.blade.php
@@ -31,7 +31,7 @@
 
 
 <button type="button" class="text-white bg-green-700 hover:bg-green-800 focus:outline-none focus:ring-4 focus:ring-green-300 font-medium rounded-full text-sm px-5 py-2.5 text-center mr-2 mb-2 dark:bg-green-600 dark:hover:bg-green-700 dark:focus:ring-green-800">
-<a href="/register_item">Add New Item</a>
+<a href="/inventory/register_item">Add New Item</a>
 </button>
 
 </center>
--- inventory/resources/views/users.blade.php
+++ inventory/resources/views/users.blade.php
@@ -30,7 +30,7 @@
 {{-- start of the page content --}}
 
 <button type="button" id="top" class="text-white bg-green-700 hover:bg-green-800 focus:outline-none focus:ring-4 focus:ring-green-300 font-medium rounded-full text-sm px-5 py-2.5 text-center mr-2 mb-2 dark:bg-green-600 dark:hover:bg-green-700 dark:focus:ring-green-800">
-<a href="/register_user">Add New User</a>
+<a href="/inventory/register_user">Add New User</a>
 </button>
 
 </center>
@@ -115,7 +115,7 @@ margin-right:10%;
                  {{-- first action to edit user --}}
                   <td scope="row" class="px-6 py-4 font-medium text-gray-900 whitespace-nowrap dark:text-white">
                       
-                       <a href="/edit_user_form? id={{ $user->user_id}}&firstname={{ $user->firstname }}&lastname={{ $user->lastname}}&email={{ $user->email}}&phone={{ $user->phone}}"> 
+                       <a href="/inventory/edit_user_form? id={{ $user->user_id}}&firstname={{ $user->firstname }}&lastname={{ $user->lastname}}&email={{ $user->email}}&phone={{ $user->phone}}"> 
                         <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke-width="1.5" stroke="currentColor" class="w-6 h-6">
                         <path stroke-linecap="round" stroke-linejoin="round" d="M16.862 4.487l1.687-1.688a1.875 1.875 0 112.652 2.652L10.582 16.07a4.5 4.5 0 01-1.897 1.13L6 18l.8-2.685a4.5 4.5 0 011.13-1.897l8.932-8.931zm0 0L19.5 7.125M18 14v4.75A2.25 2.25 0 0115.75 21H5.25A2.25 2.25 0 013 18.75V8.25A2.25 2.25 0 015.25 6H10" />
                         </svg>
@@ -124,7 +124,7 @@ margin-right:10%;
 
          {{-- second action to deactivate user--}}
                 {{-- <td scope="row" class="px-6 py-4 font-medium text-gray-900 whitespace-nowrap dark:text-white">
-                        <a href="/activate_deactivate? id={{ $user->id}}&status={{ $user->status }}">
+                        <a href="/inventory/activate_deactivate? id={{ $user->id}}&status={{ $user->status }}">
                 @if ($user->status =='active')
                     <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke-width="1.5" stroke="currentColor" class="w-6 h-6">
                       <path stroke-linecap="round" stroke-linejoin="round" d="M16.5 10.5V6.75a4.5 4.5 0 10-9 0v3.75m-.75 11.25h10.5a2.25 2.25 0 002.25-2.25v-6.75a2.25 2.25 0 00-2.25-2.25H6.75a2.25 2.25 0 00-2.25 2.25v6.75a2.25 2.25 0 002.25 2.25z" />
@@ -175,7 +175,7 @@ margin-right:10%;
         // user clicked "OK"
         
         //piece of code missing here for sending user id for deletion.
-          window.location.href = "/deleteuser? id={{ $user->user_id}}";
+          window.location.href = "/inventory/deleteuser? id={{ $user->user_id}}";
 
         } else {
 
