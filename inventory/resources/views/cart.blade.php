225e8b640c9ab798438f2cdb020b77d13f3b000f
joshuamumbere <joshuamumbere71@gmail.com>
2024-08-01 15:51:57 +0300

    Revert "Latest changes to the directory files"
    This reverts commit f28e2903e4378a4612ca18931e473a5a262b6a6d.

--- inventory/resources/views/cart.blade.php
+++ inventory/resources/views/cart.blade.php
@@ -91,7 +91,7 @@ margin-right:10%;
 <hr><br>
 <center>
 <button type="button" class="text-white bg-blue-700 hover:bg-blue-800 focus:outline-none focus:ring-4 focus:ring-blue-300 font-medium rounded-full text-sm px-5 py-2.5 text-center mr-2 mb-2 dark:bg-blue-600 dark:hover:bg-blue-700 dark:focus:ring-blue-800">
-<a href="/inventory/borrows">Back</a>
+<a href="/borrows">Back</a>
 </button>
 </center>
 <br>
